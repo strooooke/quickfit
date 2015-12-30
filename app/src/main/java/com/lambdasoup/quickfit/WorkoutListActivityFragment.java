4b08487a047a69e2e20a89b94fe4a65a59eda8a2
Juliane Lehmann <jl@lambdasoup.com>
2015-12-30 11:56:56 +0100

    Create project

---
+++ app/src/main/java/com/lambdasoup/quickfit/WorkoutListActivityFragment.java
@@ -0,0 +1,22 @@
+package com.lambdasoup.quickfit;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class WorkoutListActivityFragment extends Fragment {
+
+    public WorkoutListActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_workout_list, container, false);
+    }
+}
